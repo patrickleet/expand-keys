1287c5e81124fc67f2f5da7fa3214505b278a182
Patrick Scott <patrick.scott@zx-ventures.com>
2017-12-08 14:57:18 -0500

    feat(expand-keys): implement expand keys

---
+++ __tests__/main.js
@@ -0,0 +1,17 @@
+import { expandKeys } from 'main'
+
+describe('expandKeys', () => {
+  it('expands keys', () => {
+    let expected = {
+      hello: {
+        world: 1,
+        galaxy: 2
+      }
+    }
+    let expanded = expandKeys({
+      'hello.world': 1,
+      'hello.galaxy': 2
+    })
+    expect(expanded).toEqual(expected)
+  })
+})
---
+++ src/main.js
@@ -0,0 +1,33 @@
+export const setPathValue = (layer, path, value) => {
+  path = path.split('.')
+
+  for (let i = 0; i < path.length; i++) {
+    // if value and is last item
+    if (value != null && i + 1 === path.length) { layer[path[i]] = value }
+    layer = layer[path[i]] = layer[path[i]] || {}
+  }
+
+  return layer
+}
+
+export const filterKeysWithDots = (obj) => {
+  let keys = []
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key) && key.indexOf('.') > -1) {
+      keys.push(key)
+    }
+  }
+  return keys
+}
+
+// expands keys with .'s to nested objects
+export const expandKeys = (obj) => {
+  return filterKeysWithDots(obj)
+    .reduce(function(obj, key) {
+      var keyValue = obj[key]
+      setPathValue(obj, key, keyValue)
+      delete obj[key]
+      return obj
+    }, obj)
+}
